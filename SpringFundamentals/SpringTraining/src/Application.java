6a927b750128aaa53d7ca1edf120e0c724482c6f
IT18107074 - Upulie H.D.I <IT18107074@my.sliit.lk>
2022-07-19 20:55:50 +0530

    Updated the Spring Boot project to have the Employee bean

--- SpringFundamentals/SpringTraining/src/Application.java
+++ SpringFundamentals/SpringTraining/src/Application.java
@@ -1,14 +1,20 @@
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.krishantha.training.salesmanager.model.Employee;
 import com.krishantha.training.salesmanager.service.EmployeeService;
-import com.krishantha.training.salesmanager.service.EmployeeServiceImpl;
+//import com.krishantha.training.salesmanager.service.EmployeeServiceImpl;
 
 
 public class Application {
 
 	public static void main(String[] args) {
-		EmployeeService employeeService = new EmployeeServiceImpl();
+
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		EmployeeService employeeService = applicationContext.getBean("employeeService",EmployeeService.class);
 		List<Employee> employees = employeeService.getAllEmployees();
 		
 		for(Employee employee:employees) {
--- SpringFundamentals/SpringTraining/src/com/krishantha/training/salesmanager/service/EmployeeServiceImpl.java
+++ SpringFundamentals/SpringTraining/src/com/krishantha/training/salesmanager/service/EmployeeServiceImpl.java
@@ -4,12 +4,20 @@ import java.util.List;
 
 import com.krishantha.training.salesmanager.model.Employee;
 import com.krishantha.training.salesmanager.repository.EmployeeRepository;
-import com.krishantha.training.salesmanager.repository.HibernateEmployeeRepositoryImpl;
+//import com.krishantha.training.salesmanager.repository.HibernateEmployeeRepositoryImpl;
 
 public class EmployeeServiceImpl implements EmployeeService{
 	
-	EmployeeRepository employeeRepository = new HibernateEmployeeRepositoryImpl();
+	private EmployeeRepository employeeRepository;
 	
+	
+	
+	public void setEmployeeRepository(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+
+
+
 	@Override
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.getAllEmployees();
